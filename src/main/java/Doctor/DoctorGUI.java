43f404dd79b84881b6c3f2ab5ab46446c64a0c97
WannesVermeire <wannes.vermeiren@student.kuleuven.be>
2022-12-09 11:55:27 +0100

    Doctor GUI

--- src/main/java/Doctor/DoctorGUI.java
+++ src/main/java/Doctor/DoctorGUI.java
@@ -1,4 +1,64 @@
 package Doctor;
 
+import Interfaces.MatchingServiceInterface;
+import Interfaces.MixingProxyInterface;
+import Interfaces.RegistrarInterface;
+import Visitor.Visitor;
+
+import javax.print.Doc;
+import javax.swing.*;
+import java.awt.*;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import static Services.Methods.stringToHash;
+
 public class DoctorGUI {
+    private JFrame frame;
+    private JButton readLogsButton;
+    private JButton sendLogsButton;
+    private Doctor doctor;
+
+    public DoctorGUI(Doctor doctor) {
+        this.doctor = doctor;
+
+        frame = new JFrame("Doctor");
+        readLogsButton = new JButton("Read logs from file");
+        sendLogsButton = new JButton("Send signed logs to the server");
+
+        /******************************** 3. REGISTERING INFECTED USER **********************************/
+
+        readLogsButton.addActionListener(a -> {
+            doctor.importVisits();
+        });
+
+        sendLogsButton.addActionListener(a -> {
+            try {
+                // fire to localhost port 2300
+                Registry myRegistry = LocateRegistry.getRegistry("localhost", 2300);
+                // search for RegistrarService
+                MatchingServiceInterface impl = (MatchingServiceInterface) myRegistry.lookup("MatchingService");
+
+                // Send signedLogs to the server
+                doctor.generateSignedLogs();
+                impl.receiveSignedLogs(doctor.getSignedLogs(), doctor.getPublicKey());
+
+            } catch (Exception e) { e.printStackTrace(); }
+        });
+
+
+
+        /******************************** 3. REGISTERING INFECTED USER **********************************/
+
+
+
+
+        frame.setLayout(new FlowLayout());
+        frame.add(readLogsButton);
+        frame.setSize(250, 100);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
--- src/main/java/GlobalMain.java
+++ src/main/java/GlobalMain.java
@@ -1,3 +1,5 @@
+import Doctor.Doctor;
+import Doctor.DoctorGUI;
 import Facility.BarOwnerGUI;
 import Facility.Facility;
 import MatchingService.MatchingService;
@@ -6,6 +8,7 @@ import Registrar.Registrar;
 import Visitor.Visitor;
 import Visitor.VisitorGUI;
 
+import java.util.concurrent.TimeUnit;
 
 public class GlobalMain {
     public static void main(String[] args) throws InterruptedException {
@@ -15,14 +18,10 @@ public class GlobalMain {
 
 
         BarOwnerGUI barOwner1 = new BarOwnerGUI(new Facility("Hamann", "Vantegemstraat 3, 9230 Wetteren", "+32 9 333 77 77"));
-
-
         VisitorGUI visitor1 = new VisitorGUI(new Visitor("Wannes", "+32 456 30 81 66"));
+        DoctorGUI doctor1 = new DoctorGUI(new Doctor("Toon Eeraerts"));
 
-
-        //DoctorClient.main(args);
-
-        //MatchingService.getNyms();
+//        MatchingService.getNyms();
 
     }
 }
